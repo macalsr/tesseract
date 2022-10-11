bced65c996158f6759180e3783de03957da16a78
tp.maria.sribeiro <tp.maria.sribeiro@totvspartners.com.br>
2022-10-11 12:04:15 -0300

    conversor para tons cinzas

--- src/main/java/com/example/tesseract/utils/ImgUtil.java
+++ src/main/java/com/example/tesseract/utils/ImgUtil.java
@@ -52,11 +52,33 @@ public class ImgUtil {
         return text;
     }
 
+    public static BufferedImage convert2GreyScale(BufferedImage img) throws IOException {
+        int width = img.getWidth();
+        int height = img.getHeight();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int p = img.getRGB(x, y);
+
+                int a = (p >> 24) & 0xff;
+                int r = (p >> 16) & 0xff;
+                int g = (p >> 8) & 0xff;
+                int b = p & 0xff;
+                int avg = (r + g + b) / 3;
+
+                p = (a << 24) | (avg << 16) | (avg << 8) | avg;
+
+                img.setRGB(x, y, p);
+            }
+        }
+        return img;
+    }
+
     public static BufferedImage retiraBorda(BufferedImage file) throws IOException {
         BufferedImage img = file.getSubimage(115, 80, 965, 1190);
         String outputFiles = "src/main/resources/outputfiles/";
         ImageIO.write(img, "png", new File(outputFiles + "imagem1.png"));
-        return img;
+        return convert2GreyScale(img);
     }
 
     public static String coletaInformacoes(BufferedImage img) throws IOException {
@@ -69,17 +91,17 @@ public class ImgUtil {
         BufferedImage codigoDeVerificacao = img.getSubimage(767, 89, 161, 38);
 
         //Prestador de serviços
-        BufferedImage prestadorServiços = img.getSubimage(117, 135, 725, 117);
+        BufferedImage prestadorServicos = img.getSubimage(117, 135, 725, 117);
 
         //Serve somente para demostração, não faz diferença
         String outputFiles = "src/main/resources/outputfiles/";
         ImageIO.write(numeroNota, "png", new File(outputFiles + "imagem2.png"));
         ImageIO.write(codigoDeVerificacao, "png", new File(outputFiles + "imagem3.png"));
-        ImageIO.write(prestadorServiços, "png", new File(outputFiles + "imagem4.png"));
+        ImageIO.write(prestadorServicos, "png", new File(outputFiles + "imagem4.png"));
 
         info.add(leitorImagem(numeroNota));
         info.add(leitorImagem(codigoDeVerificacao));
-        info.add(leitorImagem(prestadorServiços));
+        info.add(leitorImagem(prestadorServicos));
 
         return info.toString();
     }
